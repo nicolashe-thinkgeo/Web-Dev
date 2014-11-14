5b4711809994ea1edfa87894c71490e247fb79ca
schooloffish <liuxun@outlook.com>
2014-11-14 17:22:15 +0800

    Revert "Revert "Web Dev""
    This reverts commit af9fdfaded092d67ca871dc50b0e3e65511b5a71.

---
+++ NodeJs test/server.js
@@ -0,0 +1,10 @@
+var http=require('http');
+
+http.createServer(function(request,response){    
+	response.writeHead(200,{'Content-Type':'application/json',"Access-Control-Allow-Origin": "*"});
+	response.write(JSON.stringify(
+	[{name:'Liu Xun',age:27},{name:'Feifei',age:1}]
+	));
+	response.end();
+}).listen(8888)
+console.log('Server running at http://localhost:8888/');
